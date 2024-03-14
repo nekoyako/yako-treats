83a8f2dc80c964caee5ebd062082fd8ef9a63b87
Nico Cristian <nicocristian@Nicos-MacBook-Air.local>
2024-03-14 15:59:05 +0700

    trial

--- php/config.php
+++ php/config.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost", "id20889410_uaspwd2023", "Uaspwd2023*", "id20889410_uas_yako") or die("Couldn't connect: " . mysqli_connect_error());
+$con = mysqli_connect("127.0.0.1", "id20889410_uaspwd2023", "Uaspwd2023*", "id20889410_uas_yako") or die("Couldn't connect: " . mysqli_connect_error());
 
 ?>
